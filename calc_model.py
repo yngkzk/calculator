52a0bf3bfdcbe4f73bfcd636b539f0ed7d5163ed
yngkzk <ansar.iskakow@gmail.com>
2023-09-02 22:47:39 +0600

    Fixed SimpleCalcModel

--- calc_model.py
+++ calc_model.py
@@ -1,5 +1,5 @@
 class SimpleCalcModel:
-    __display = '2+2*2'
+    __display = '0'
 
     def calculate(self):
         try:
@@ -14,10 +14,23 @@ class SimpleCalcModel:
                 if self.__display == "0":
                     self.__display = key
                 else:
-                    self.__display += key
+                    if key == "0" or key == "00":
+                        if self.__display[-1] not in "+-*/":
+                            if self.__display[-1] == '.':
+                                self.__display += key
+                            elif int(self.__display[-1]) > 0:
+                                self.__display += key
+                    else:
+                        self.__display += key
             else:
                 if self.__display[-1] not in "+-*/":
-                    self.__display += key
+                    if key == '(':
+                        self.__display += "*" + key
+                    elif key == ')':
+                        if self.__display.count('(') > self.__display.count(')'):
+                            self.__display += key
+                    else:
+                        self.__display += key
 
             if key == "C":
                 if len(self.__display) > 1:
@@ -25,7 +38,18 @@ class SimpleCalcModel:
 
             if key == "AC":
                 self.__display = "0"
+
+            if key == ".":
+                if "." in self.__display:
+                    self.__display = self.__display
+                else:
+                    self.__display += key
+
         else:
+            if '(' in self.__display and ')' not in self.__display:
+                self.__display += ')'
+            elif self.__display[-1] == '(':
+                self.__display = self.__display[:-2]
             print(self.__display)
             self.calculate()
 
@@ -39,9 +63,18 @@ if __name__ == '__main__':
 
     calc.command('3')
     calc.command('+')
-    calc.command('+')
     calc.command('7')
+    calc.command('00')
+    calc.command('.')
+    calc.command('0')
+    calc.command('5')
+    calc.command('(')
+    calc.command('23')
+    calc.command('+')
+    calc.command('52')
+    calc.command(')')
+    calc.command('(')
+    print(calc.get_display())
     calc.command('=')
-    calc.calculate()
 
     print(calc.get_display())
