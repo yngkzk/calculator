9c56d33b1a0c027937d80786d0e64b44c0ccc9e1
yngkzk <ansar.iskakow@gmail.com>
2023-09-03 14:24:19 +0600

    Project | Calculator

--- calc_model.py
+++ calc_model.py
@@ -1,3 +1,6 @@
+import re
+
+
 class SimpleCalcModel:
     __display = '0'
 
@@ -25,7 +28,14 @@ class SimpleCalcModel:
             else:
                 if self.__display[-1] not in "+-*/":
                     if key == '(':
-                        self.__display += "*" + key
+                        if self.__display == '0':
+                            self.__display = key
+                        elif self.__display[-1] == '.':
+                            self.__display = self.__display[:-1] + "*" + key
+                        elif self.__display[-1].isdigit() or self.__display[-1] == ')':
+                            self.__display += "*" + key
+                        else:
+                            self.__display += key
                     elif key == ')':
                         if self.__display.count('(') > self.__display.count(')'):
                             self.__display += key
@@ -46,8 +56,14 @@ class SimpleCalcModel:
                     self.__display += key
 
         else:
-            if '(' in self.__display and ')' not in self.__display:
-                self.__display += ')'
+            if self.__display.count('(') > self.__display.count(')'):
+                self.__display += ')' * int(self.__display.count('(') - self.__display.count(')'))
+                index = self.__display.rfind('(')
+                numbers = re.split(r'[ +\-*/\()]+', self.__display)
+                self.__display = list(self.__display)
+                self.__display.insert(index + 1, numbers[-2])
+                self.__display = ''.join(self.__display)
+
             elif self.__display[-1] == '(':
                 self.__display = self.__display[:-2]
             print(self.__display)
@@ -61,20 +77,20 @@ if __name__ == '__main__':
     print('Testing model:')
     calc = SimpleCalcModel()
 
+    calc.command('(')
+    calc.command('9')
+    calc.command('-')
+    calc.command('7')
+    calc.command(')')
+    calc.command('.')
+    calc.command('(')
     calc.command('3')
     calc.command('+')
-    calc.command('7')
-    calc.command('00')
-    calc.command('.')
-    calc.command('0')
     calc.command('5')
-    calc.command('(')
-    calc.command('23')
-    calc.command('+')
-    calc.command('52')
     calc.command(')')
     calc.command('(')
     print(calc.get_display())
     calc.command('=')
 
     print(calc.get_display())
+    calc.command('=')
