5ef8bc6abf5ba0b693fc989294560949aea03404
ChiriNNi <103880918+ChiriNNi@users.noreply.github.com>
2023-09-05 19:54:57 +0600

    Switcher

--- calc_main_window.py
+++ calc_main_window.py
@@ -8,15 +8,21 @@ class CalculatorMainWindow(QMainWindow):
     def __init__(self, title):
         super().__init__()
         self.setWindowTitle(title)
-        main_widget = QWidget()
-        self.calc_layout = QGridLayout()
+        main_widget = QWidget() 
+        self.calc_layout = QGridLayout() 
         main_widget.setLayout(self.calc_layout)
         self.setCentralWidget(main_widget)
 
     def set_view(self, view):
+        if self.calc_view: 
+            self.calc_view.hide() 
         self.calc_view = view
         self.calc_layout.addWidget(self.calc_view, 1, 0)
+    
+    def set_model(self, model): 
+        self.calc_model = model 
+        if self.calc_view: 
+            self.calc_view.set_model(model)
 
-    def set_switcher(self, widget):
+    def set_switch(self, widget): 
         self.calc_layout.addWidget(widget, 0, 0)
-
